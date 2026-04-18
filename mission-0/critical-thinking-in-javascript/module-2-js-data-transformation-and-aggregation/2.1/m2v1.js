8f26ab9b19a58a6ed9e9b8a39297663da4738e14
souravxdev <sourav.seu.cse@gmail.com>
2026-04-18 23:04:01 +0600

    0.2.1 started learning module 2

---
+++ mission-0/critical-thinking-in-javascript/module-2-js-data-transformation-and-aggregation/2.1/m2v1.js
@@ -0,0 +1,8 @@
+const obj = {
+    nextLevel: {courseId: 'level2'},
+    "Programming Hero": {courseId: 'level1'},
+    true: {courseId: 'phitron'}
+}
+console.log(obj.nextLevel)
+console.log(obj["nextLevel"])
+console.log(obj["Programming Hero"])
